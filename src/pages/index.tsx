6d701ce1657ca5d464b0936095ebd15492c77d95
Fernando Souza <l.fernando.dev@gmail.com>
2023-07-21 17:56:46 -0300

    add styles for home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,29 @@
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+
+import camisa1 from '../assets/1.png'
+import camisa2 from '../assets/2.png'
+import camisa3 from '../assets/3.png'
+
 export default function Home() {
   return (
-    <div>
-      <h1>Teste</h1>
-    </div>
+    <HomeContainer>
+      <Product>
+        <Image src={camisa1} alt="" width={520} height={480}/>
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camisa2} alt="" width={520} height={480}/>
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
