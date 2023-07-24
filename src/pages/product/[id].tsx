3c27ed36d851cffd553efac783cf40d39e9398df
Fernando Souza <l.fernando.dev@gmail.com>
2023-07-23 23:18:26 -0300

    finhised product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,14 +1,71 @@
 import { useRouter } from 'next/router'
 import React from 'react'
+import { ImageContainer, ProductContainer, ProductDetail } from '../../styles/pages/product'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { stripe } from '../../lib/stripe'
+import Stripe from 'stripe'
+import Image from 'next/image'
 
-const Product = () => {
+interface ProductProps {
+  product: {
+    id: string
+    name: string
+    imageUrl: string
+    price: string
+    description: string
+  }
+}
 
-  const {query} = useRouter()
-  console.log(query)
+export default function Product({product}: ProductProps) {
+  const { isFallback, query } = useRouter()
 
   return (
-    <div>Product</div>
+    <ProductContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={480} />
+      </ImageContainer>
+
+      <ProductDetail>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+
+        <p>{product.description}</p>
+
+        <button>Comprar agora</button>
+      </ProductDetail>
+    </ProductContainer>
   )
 }
 
-export default Product
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking', //true
+  }
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+  const productId = params.id
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price'],
+  })
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-br', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(price.unit_amount / 100),
+        description: product.description
+      }
+    },
+    revalidate: 60 * 60 * 1
+  }
+}
