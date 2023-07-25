d923f77146213264d771e9878c98938f8f94be08
Fernando Souza <l.fernando.dev@gmail.com>
2023-07-24 22:53:24 -0300

    add payment method for buy now

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -13,11 +13,16 @@ interface ProductProps {
     imageUrl: string
     price: string
     description: string
+    defaultPriceId: string
   }
 }
 
 export default function Product({product}: ProductProps) {
   const { isFallback, query } = useRouter()
+
+  function handlePriceId() {
+    console.log(product.defaultPriceId)
+  }
 
   return (
     <ProductContainer>
@@ -31,7 +36,7 @@ export default function Product({product}: ProductProps) {
 
         <p>{product.description}</p>
 
-        <button>Comprar agora</button>
+        <button onClick={handlePriceId}>Comprar agora</button>
       </ProductDetail>
     </ProductContainer>
   )
@@ -63,7 +68,8 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
           style: 'currency',
           currency: 'BRL',
         }).format(price.unit_amount / 100),
-        description: product.description
+        description: product.description,
+        defaultPriceId: price.id
       }
     },
     revalidate: 60 * 60 * 1
